af2cdb94c2485dd857208ef2e9c264f3a4649f5b
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-08-16 18:38:24 -0500

    nuevo commit

---
+++ src/main/java/com/sebastian/AA1_EV01/Aa1Ev01Application.java
@@ -0,0 +1,13 @@
+package com.sebastian.AA1_EV01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa1Ev01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aa1Ev01Application.class, args);
+	}
+
+}
